8115a02105165f673a7fd9f68a2dd5fe5a12476d
laureduchemin <duchemin_laure@yahoo.fr>
2016-04-02 21:49:53 +0200

    Création page connexion
    Plus mise en page site avec css

--- WebContent/WEB-INF/PageConnexion.jsp
+++ WebContent/WEB-INF/PageConnexion.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title> Page Inscription </title>
+<title>Page Connexion</title>
 <link type="text/css" rel="stylesheet" href="form.css" />
 </head>
 <body>
@@ -12,32 +12,31 @@
 <div id="bandeau"><p align="center"><font size="3" face="cursive" color="black"><b>Astuces Neko Atsume</b></font></p></div>
 <div id="menu">Menu</div>
 <div id="contenu">
-<form method="post" action="inscription">
-            <fieldset>
-                <legend>Inscription</legend>
-                <p>Vous pouvez vous inscrire via ce formulaire.</p>
-                
-                <label for="pseudo">Pseudo <span class="requis">*</span></label>
-                <input type="text" id="pseudo" name="pseudo" value="" size="20" maxlength="20" />
-                <br />
 
-                <label for="email">Adresse email <span class="requis">*</span></label>
-                <input type="text" id="email" name="email" value="" size="20" maxlength="60" />
+<form method="post" action="connexion">
+            <fieldset>
+<legend>Connexion</legend>
+                <p>Vous pouvez vous connecter via ce formulaire.</p>
+
+                <label for="pseudo">Pseudo <span class="requis">*</span></label>
+                <input type="text" id="email" name="pseudo" value="" size="20" maxlength="60" />
+                <span class="erreur">${form.erreurs['pseudo']}</span>
                 <br />
 
                 <label for="motdepasse">Mot de passe <span class="requis">*</span></label>
                 <input type="password" id="motdepasse" name="motdepasse" value="" size="20" maxlength="20" />
+                <span class="erreur">${form.erreurs['motdepasse']}</span>
                 <br />
 
-                <label for="confirmation">Confirmation du mot de passe <span class="requis">*</span></label>
-                <input type="password" id="confirmation" name="confirmation" value="" size="20" maxlength="20" />
+                <input type="submit" value="Connexion" class="sansLabel" />
                 <br />
-
-                <input type="submit" value="Inscription" class="sansLabel" />
-                <br />
+                
+                <p class="${empty form.erreurs ? 'succes' : 'erreur'}">${form.resultat}</p>
             </fieldset>
         </form>
         </div>
+        
+        
         <div id="piedpage"></div>
 
 </body>
---
+++ src/com/form/TraitementConnexion.java
@@ -0,0 +1,108 @@
+/**
+ * 
+ */
+package com.form;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.beans.Utilisateur;
+
+/**
+ * @author laureduchemin
+ *
+ */
+public final class TraitementConnexion {
+
+	private static final String CHAMP_PSEUDO  = "pseudo";
+    private static final String CHAMP_PASS   = "mdp";
+    private String resultat;
+    private Map<String, String> erreurs = new HashMap<String, String>();
+    
+    public String getResultat() {
+        return resultat;
+    }
+    
+    public Map<String, String> getErreurs() {
+        return erreurs;
+    }
+    
+    public Utilisateur connecterUtilisateur( HttpServletRequest request ) {
+        /** Récupération des champs du formulaire */
+        String pseudo = getValeurChamp( request, CHAMP_PSEUDO );
+        String mdp = getValeurChamp( request, CHAMP_PASS );
+
+        Utilisateur utilisateur = new Utilisateur();
+
+        /** Validation du champ email. */
+        try {
+            validationPseudo( pseudo );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_PSEUDO, e.getMessage() );
+        }
+        utilisateur.setEmail( pseudo );
+
+        /** Validation du champ mot de passe. */
+        try {
+            validationMotDePasse( mdp );
+        } catch ( Exception e ) {
+            setErreur( CHAMP_PASS, e.getMessage() );
+        }
+        utilisateur.setMdp( mdp );
+
+        /** Initialisation du résultat global de la validation. */
+        if ( erreurs.isEmpty() ) {
+            resultat = "Succès de la connexion.";
+        } else {
+            resultat = "Échec de la connexion.";
+        }
+
+        return utilisateur;
+    }
+    
+    /**
+     * Valide l'adresse email saisie.
+     */
+    private void validationPseudo( String pseudo ) throws Exception {
+        if ( pseudo != null ) {
+        	 if ( pseudo.length() < 3 ) {
+            throw new Exception( "Merci de saisir un pseudo valide." );
+        }
+        }
+    }
+    
+    /**
+     * Valide le mot de passe saisi.
+     */
+    private void validationMotDePasse( String motDePasse ) throws Exception {
+        if ( motDePasse != null ) {
+            if ( motDePasse.length() < 3 ) {
+                throw new Exception( "Le mot de passe doit contenir au moins 3 caractères." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir votre mot de passe." );
+        }
+    }
+    
+    /**
+     * Ajoute un message correspondant au champ spécifié à la map des erreurs.
+     */
+    private void setErreur( String champ, String message ) {
+        erreurs.put( champ, message );
+    }
+    
+    /**
+     * Méthode utilitaire qui retourne null si un champ est vide, et son contenu
+     * sinon.
+     */
+    private static String getValeurChamp( HttpServletRequest request, String nomChamp ) {
+        String valeur = request.getParameter( nomChamp );
+        if ( valeur == null || valeur.trim().length() == 0 ) {
+            return null;
+        } else {
+            return valeur;
+        }
+    }
+}
