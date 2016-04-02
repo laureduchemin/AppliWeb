951a17569f8c313f4821c54b6c1075f8d0022651
laureduchemin <duchemin_laure@yahoo.fr>
2016-04-02 22:02:52 +0200

    Deconnexion
    Ajout servlet gérant la déconnexion (qui se fait au bout de 30 min)
    puis redirection vers la page de connexion

---
+++ src/com/servlets/Deconnexion.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author laureduchemin
+ *
+ */
+public class Deconnexion extends HttpServlet {
+
+	
+	public static final String VUE = "/connexion";
+	
+	/**
+	 * Mettre lien de redirection
+	 */
+	public static final String URL_REDIRECTION = "";
+
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /** Récupération et destruction de la session en cours */
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
+}
