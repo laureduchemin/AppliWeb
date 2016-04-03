9b7272ffdaa0c036f6b34ba0ad217b9af648719a
laureduchemin <duchemin_laure@yahoo.fr>
2016-04-03 15:44:33 +0200

    Gestion session

--- src/com/servlets/PageEditProfil.java
+++ src/com/servlets/PageEditProfil.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author laureduchemin
@@ -19,13 +20,23 @@ public class PageEditProfil extends HttpServlet {
 	/**
 	 * 
 	 */
+	public static final String RED_LOGIN_USR = "/PageConnexion";
 	private static final long serialVersionUID = 1L;
 	public static final String VUE = "/WEB-INF/PageEditProfil.jsp";
+	public static final String ATT_SESSION_USER = "sessionUtilisateur";
 	
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 
 
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    	HttpSession session = request.getSession();
+    	
+    	if ( session.getAttribute( ATT_SESSION_USER ) == null ) {
+            /* Redirection vers la page publique */
+            response.sendRedirect( request.getContextPath() + RED_LOGIN_USR );
+        } else {
+            /* Affichage de la page restreinte */
+            this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        }
         
     }
 
