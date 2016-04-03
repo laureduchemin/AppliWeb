455130c25ac1b100d1805ffc8806d74274aeaa2b
laureduchemin <duchemin_laure@yahoo.fr>
2016-04-03 15:22:26 +0200

    Ajout méthode de redirection si usr pas connecté

--- src/com/servlets/PageProfil.java
+++ src/com/servlets/PageProfil.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author laureduchemin
@@ -20,14 +21,20 @@ public class PageProfil extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	public static final String RED_LOGIN_USR = "/PageConnexion";
 	public static final String VUE = "/WEB-INF/PageProfil.jsp";
+	public static final String ATT_SESSION_USER = "sessionUtilisateur";
 	
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
 
-
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-        
+    	HttpSession session = request.getSession();
+    	
+    	if ( session.getAttribute( ATT_SESSION_USER ) == null ) {
+            /* Redirection vers la page publique */
+            response.sendRedirect( request.getContextPath() + RED_LOGIN_USR );
+        } else {
+            /* Affichage de la page restreinte */
+            this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        }        
     }
-
-
 }
